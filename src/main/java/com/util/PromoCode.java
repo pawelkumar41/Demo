8157f2338b609472300f553ff0df75a918127917
Pawel kumar <pawel.kumar@revv.co.in>
2017-10-26 15:04:26 +0530

    Promo fix

--- src/main/java/com/util/PromoCode.java
+++ src/main/java/com/util/PromoCode.java
@@ -13,9 +13,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+
 import com.common.commons;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -25,15 +27,24 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class PromoCode {
 
+    /**
+     * @param args
+     * @throws Exception
+     */
+
+
+    public static DefaultHttpClient httpClient = new DefaultHttpClient();
+    static HashMap<String, String> has = new HashMap<String, String>();
     private static String customerid = "";
     private static String accessToken = "";
-    private static String c_accessToken="";
+    private static String c_accessToken = "";
     private static String endDate = "";
     private static String startDate = "";
     private static String longitude1 = "";
@@ -42,21 +53,13 @@ public class PromoCode {
     private static String latitude2 = "";
     private static String carModelId = "";
     private static String model;
-    private static String bookingIDForCustomer="";
+    private static String bookingIDForCustomer = "";
     private static String _id = "";
     private static String adminid = "";
     private static String priceInfoId = "";
-    private static String priceInfoId1="";
-    private static int promoCodeAmount=0;
-    private static int adjustAmount=0;
-    static HashMap<String, String> has = new HashMap<String, String>();
-    /**
-     * @param args
-     * @throws Exception
-     */
-
-
-    public static DefaultHttpClient httpClient = new DefaultHttpClient();
+    private static String priceInfoId1 = "";
+    private static int promoCodeAmount = 0;
+    private static int adjustAmount = 0;
 
     @Test(priority = 1)
     public static void adminLogin() throws Exception {
@@ -136,7 +139,7 @@ public class PromoCode {
             String resMessage = object.getString("message");
             object1 = object.getJSONObject("data");
             customerid = object1.getString("customerID");
-            c_accessToken=object1.getString("accessToken");
+            c_accessToken = object1.getString("accessToken");
             System.out.println("++Pawel++" + customerid);
 
         } finally
@@ -215,11 +218,11 @@ public class PromoCode {
             object.put("customerID", customerid);
             object.put("endDate", userDetails.get("enddate"));
             object.put("lat", userDetails.get("latitude1"));
-            object.put("leadSource","WEB");
-            object.put("lng",userDetails.get("longitude1"));
+            object.put("leadSource", "WEB");
+            object.put("lng", userDetails.get("longitude1"));
             object.put("pricingType", 2);
-            object.put("promoCodeName","rv10");
-            object.put("startDate",userDetails.get("startdate"));
+            object.put("promoCodeName", "rv10");
+            object.put("startDate", userDetails.get("startdate"));
             String message;
             message = object.toString();
             postRequest.setEntity(new StringEntity(message, "UTF8"));
@@ -274,7 +277,7 @@ public class PromoCode {
             object.put("bookingID", "0");
             object.put("adminID", adminid);
             object.put("useRevvCredit", false);
-            object.put("promoCodeName","RV10");
+            object.put("promoCodeName", "RV10");
             String message;
             message = object.toString();
             postRequest.setEntity(new StringEntity(message, "UTF8"));
@@ -291,9 +294,9 @@ public class PromoCode {
             }
             String resMessage = object1.getString("message");
             object1 = object1.getJSONObject("data");
-            priceInfoId=object1.getString("priceInfoId");
+            priceInfoId = object1.getString("priceInfoId");
             adjustAmount = object1.getInt("adjustAmount");
-            System.out.print("Pawel Kumar Dalal"+priceInfoId+"Pawel Kumar Dalal"+adjustAmount);
+            System.out.print("Pawel Kumar Dalal" + priceInfoId + "Pawel Kumar Dalal" + adjustAmount);
         } finally
 
         {
@@ -357,5 +360,168 @@ public class PromoCode {
 
     }
 
+    @Test(priority = 7)
+
+    public static void getCarModifyPromoInfo() throws Exception {
+
+        try
+
+        {
+            HashMap<String, String> bookingdetails;
+            bookingdetails = commons.getHashmapfromtxt("weekdayModify.txt");
+            HttpGet getRequest = new HttpGet("http://staging.admin.revv.co.in/api/v2/carInfo/startDate=" + bookingdetails.get("startdate") + "&endDate=" + bookingdetails.get("enddate") + "&longitude1=" + bookingdetails.get("longitude1") + "&latitude1=" + bookingdetails.get("latitude1") + "&longitude2=" + bookingdetails.get("longitude2") + "&latitude2=" + bookingdetails.get("latitude2") + "&carInfoID=0&bookingId=" + bookingIDForCustomer + "?" + "customerID=" + customerid);
+            getRequest.addHeader("content-type", "application/json");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            HttpResponse response = httpClient.execute(getRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode + "Pawel Status Code");
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            StringBuilder output = new StringBuilder();
+            String out;
+            while ((out = br.readLine()) != null) {
+                output.append(out);
+            }
+            String finalOutput = output.toString();
+            JSONObject obj = new JSONObject(finalOutput);
+            obj = obj.getJSONObject("data");
+            System.out.println("++++++++++++");
+            org.json.JSONArray arr = obj.getJSONArray("carModels");
+            HashMap<String, String> id = new HashMap<String, String>();
+            // for(int i=0;i<arr.length();i++){
+            JSONObject obj2 = arr.getJSONObject(0);
+            obj2.toString();
+            System.out.println(obj2);
+            carModelId = obj2.getString("_id");
+            model = obj2.getString("model");
+            try {
+                id.put(obj2.getString("_id"), obj2.getString("model"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(carModelId);
+            System.out.println(model);
+
+        } finally
+
+        {
+            //Important: Close the connect
+            //  httpClient.getConnectionManager().shutdown();
+        }
+
+
+    }
+
+    @Test(priority = 8)
+
+    public static void getPriceInfoModifyPromo() throws Exception {
+
+        try
+
+        {
+            HashMap<String, String> bookingdetails = null;
+            bookingdetails = commons.getHashmapfromtxt("weekdayModify.txt");
+            //Define a postRequest request
+            HttpPost postRequest = new HttpPost("http://staging.admin.revv.co.in/api/v1/booking/setPriceInfo");
+            System.out.println(postRequest);
+            postRequest.addHeader("content-type", "application/json");
+            JSONObject object = new JSONObject();
+            object.put("customerID", customerid);
+            object.put("accessToken", accessToken);
+            object.put("carModelID", carModelId);
+            object.put("latitude", bookingdetails.get("latitude1"));
+            object.put("longitude", bookingdetails.get("longitude1"));
+            object.put("startDate", bookingdetails.get("startdate"));
+            object.put("endDate", bookingdetails.get("enddate"));
+            object.put("promoCodeName", "RV10");
+            object.put("deviceType", bookingdetails.get("panel"));
+            object.put("pricingType", 2);
+            object.put("bookingID", bookingIDForCustomer);
+            object.put("adminID", adminid);
+            object.put("useRevvCredit", false);
+            String message;
+            message = object.toString();
+            postRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(postRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+            JSONObject object1 = null;
+            while ((output = br.readLine()) != null) {
+                object1 = new JSONObject(output);
+            }
+            String resMessage = object1.getString("message");
+            object1 = object1.getJSONObject("data");
+            priceInfoId1 = object1.getString("priceInfoId");
+        } finally
+
+        {
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+    @Test(priority = 9)
+
+
+    public static void modifyPromoBooking() throws Exception {
+
+        try
+
+        {
+            HashMap<String, String> bookingdetails = null;
+            bookingdetails = commons.getHashmapfromtxt("weekdayModify.txt");
+            HttpPut putRequest = new HttpPut("http://staging.admin.revv.co.in/api/booking/modifyBookingByAdmin");
+            putRequest.addHeader("content-type", "application/json");
+            JSONObject object = new JSONObject();
+            object.put("customerID", customerid);
+            object.put("accessToken", accessToken);
+            object.put("carModelID", carModelId);
+            object.put("latitude1", bookingdetails.get("latitude1"));
+            object.put("latitude2", bookingdetails.get("latitude2"));
+            object.put("longitude1", bookingdetails.get("longitude1"));
+            object.put("longitude2", bookingdetails.get("longitude2"));
+            object.put("startDate", bookingdetails.get("startdate"));
+            object.put("endDate", bookingdetails.get("enddate"));
+            object.put("bookingID", bookingIDForCustomer);
+            object.put("adminID", adminid);
+            object.put("pickUpLocation1", bookingdetails.get("pickUpLocation1"));
+            object.put("pickUpLocation2", bookingdetails.get("pickUpLocation2"));
+            object.put("priceInfoID", priceInfoId1);
+            String message;
+            message = object.toString();
+            putRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(putRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+            JSONObject object1 = null;
+            while ((output = br.readLine()) != null) {
+                object1 = new JSONObject(output);
+            }
+            String resMessage = object1.getString("message");
+            object1 = object1.getJSONObject("data");
+            //bookingIDForCustomer = object1.getString("bookingIDForCustomer");
+            System.out.println("++Pawel++" + bookingIDForCustomer);
+        } finally
+
+        {
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+
+
+    }
 
 }
