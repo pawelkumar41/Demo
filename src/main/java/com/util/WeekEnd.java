66bc4f5cc5895e6918bd741d498d7789a74be372
Pawel kumar <pawel.kumar@revv.co.in>
2018-05-27 14:15:02 +0530

    report fix

--- src/main/java/com/util/WeekEnd.java
+++ src/main/java/com/util/WeekEnd.java
@@ -2,7 +2,6 @@ package com.util;
 
 import com.common.Commons;
 import com.common.Logins;
-import org.apache.commons.logging.Log;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -11,6 +10,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
@@ -43,11 +43,11 @@ public class WeekEnd {
 
     public static DefaultHttpClient httpClient = new DefaultHttpClient();
 
-    @Test(priority = 1)
+    /*@Test(priority = 1)
 
     public static void login() throws Exception {
         Logins.main();
-    }
+    }*/
 
     @Test(priority = 2)
     //Below method is used to fetch cars available
