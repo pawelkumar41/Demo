e7d2a382225a9430591eea2f5dc351eed78ac078
Pawel kumar <pawel.kumar@revv.co.in>
2018-06-05 16:06:01 +0530

    DDW check across cities

--- src/main/java/com/Database/MongoConnection.java
+++ src/main/java/com/Database/MongoConnection.java
@@ -95,6 +95,7 @@ public class MongoConnection {
         query1.put("serviceCityID",new ObjectId("5614f2caed142d0983f9124d"));
         query1.put("rateCardVersion",rateCardVersion1);
         query1.put("doorStepDeliveryCharges",NCR1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
         DBCursor cursor1 = coll2.find(query1);
         System.out.println(query1);
         //System.out.println(coll1.find(query).count());
@@ -102,5 +103,357 @@ public class MongoConnection {
         System.out.println(delivery_chk);
         Assert.assertTrue(total_count==delivery_chk);
     }
+
+    //Verify door step for Jaipur in current version
+    public static void doorStepJaipur() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("5876295db034386fbe3ac236"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Jaipur = bookingdetails.get("Jaipur");
+        int Jaipur1=Integer.parseInt(Jaipur);
+        query1.put("serviceCityID",new ObjectId("5876295db034386fbe3ac236"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Jaipur1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Chandigarh in current version
+    public static void doorStepChandigarh() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("57725862e61f902b24096191"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Chandigarh = bookingdetails.get("Chandigarh");
+        int Chandigarh1=Integer.parseInt(Chandigarh);
+        query1.put("serviceCityID",new ObjectId("57725862e61f902b24096191"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Chandigarh1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Hyderabad in current version
+    public static void doorStepHyderabad() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("5657235b7dd9cd5d0c006cdf"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Hyderabad = bookingdetails.get("Hyderabad");
+        int Hyderabad1=Integer.parseInt(Hyderabad);
+        query1.put("serviceCityID",new ObjectId("5657235b7dd9cd5d0c006cdf"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Hyderabad1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Chennai in current version
+    public static void doorStepChennai() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("58760f57ae36000b38c8dfdb"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Chennai = bookingdetails.get("Chennai");
+        int Chennai1=Integer.parseInt(Chennai);
+        query1.put("serviceCityID",new ObjectId("58760f57ae36000b38c8dfdb"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Chennai1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Bangalore in current version
+    public static void doorStepBangalore() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("568e2bafe604ffb655acb74c"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Bangalore = bookingdetails.get("Bangalore");
+        int Bangalore1=Integer.parseInt(Bangalore);
+        query1.put("serviceCityID",new ObjectId("568e2bafe604ffb655acb74c"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Bangalore1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Mumbai in current version
+    public static void doorStepMumbai() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("57066610c7cdb98d13ed382a"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Mumbai = bookingdetails.get("Mumbai");
+        int Mumbai1=Integer.parseInt(Mumbai);
+        query1.put("serviceCityID",new ObjectId("57066610c7cdb98d13ed382a"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Mumbai1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Pune in current version
+    public static void doorStepPune() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("58d51d1415ae23c461c85adb"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Pune = bookingdetails.get("Pune");
+        int Pune1=Integer.parseInt(Pune);
+        query1.put("serviceCityID",new ObjectId("58d51d1415ae23c461c85adb"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Pune1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Coimbatore in current version
+    public static void doorStepCoimbatore() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("59c178945a8641619e3ef17b"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Coimbatore = bookingdetails.get("Coimbatore");
+        int Coimbatore1=Integer.parseInt(Coimbatore);
+        query1.put("serviceCityID",new ObjectId("59c178945a8641619e3ef17b"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Coimbatore1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Vizag in current version
+    public static void doorStepVizag() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("58e393e52267bd96537a2e9f"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Vizag = bookingdetails.get("Vizag");
+        int Vizag1=Integer.parseInt(Vizag);
+        query1.put("serviceCityID",new ObjectId("58e393e52267bd96537a2e9f"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Vizag1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+    //Verify door step for Mysore in current version
+    public static void doorStepMysore() throws FileNotFoundException {
+        HashMap<String, String> bookingdetails;
+        bookingdetails = Commons.getHashmapfromtxt("rateCard.txt");
+        MongoClient mongoClient = new MongoClient("13.126.78.55", 27017);
+        DB db = mongoClient.getDB("drive_car");
+        // query to get total count
+        DBCollection coll1=db.getCollection("ratecards");
+        BasicDBObject query = new BasicDBObject();
+        String rateCardVersion=bookingdetails.get("rateCardVersion");
+        int rateCardVersion1=Integer.parseInt(rateCardVersion);
+        query.put("serviceCityID",new ObjectId("59c16f1d0dfb39078a1baa81"));
+        query.put("rateCardVersion",rateCardVersion1);
+        query.put("pricingType",new BasicDBObject("$ne", 5));
+        System.out.println(query);
+        DBCursor cursor = coll1.find(query);
+        int total_count = coll1.find(query).count();
+        System.out.println(total_count);
+
+        DBCollection coll2=db.getCollection("ratecards");
+        BasicDBObject query1 = new BasicDBObject();
+        String Mysore = bookingdetails.get("Mysore");
+        int Mysore1=Integer.parseInt(Mysore);
+        query1.put("serviceCityID",new ObjectId("59c16f1d0dfb39078a1baa81"));
+        query1.put("rateCardVersion",rateCardVersion1);
+        query1.put("doorStepDeliveryCharges",Mysore1);
+        query1.put("pricingType",new BasicDBObject("$ne", 5));
+        DBCursor cursor1 = coll2.find(query1);
+        System.out.println(query1);
+        //System.out.println(coll1.find(query).count());
+        int delivery_chk = coll2.find(query1).count();
+        System.out.println(delivery_chk);
+        Assert.assertTrue(total_count==delivery_chk);
+    }
+
+
 
 }
--- src/main/java/com/util/VerifyRateCard.java
+++ src/main/java/com/util/VerifyRateCard.java
@@ -12,4 +12,54 @@ public class VerifyRateCard {
         MongoConnection.doorStepNCR();
     }
 
+    @Test
+    public static void ddwJaipur() throws FileNotFoundException {
+        MongoConnection.doorStepJaipur();
+    }
+
+    @Test
+    public static void ddwChandigarh() throws FileNotFoundException {
+        MongoConnection.doorStepChandigarh();
+    }
+
+    @Test
+    public static void ddwHyderabad() throws FileNotFoundException {
+        MongoConnection.doorStepHyderabad();
+    }
+
+    @Test
+    public static void ddwChennai() throws FileNotFoundException {
+        MongoConnection.doorStepChennai();
+    }
+
+    @Test
+    public static void ddwBangalore() throws FileNotFoundException {
+        MongoConnection.doorStepBangalore();
+    }
+
+    @Test
+    public static void ddwMumbai() throws FileNotFoundException {
+        MongoConnection.doorStepMumbai();
+    }
+
+    @Test
+    public static void ddwPune() throws FileNotFoundException {
+        MongoConnection.doorStepPune();
+    }
+
+    @Test
+    public static void ddwCoimbatore() throws FileNotFoundException {
+        MongoConnection.doorStepCoimbatore();
+    }
+
+    @Test
+    public static void ddwVizag() throws FileNotFoundException {
+        MongoConnection.doorStepVizag();
+    }
+
+    @Test
+    public static void ddwMysore() throws FileNotFoundException {
+        MongoConnection.doorStepMysore();
+    }
+
 }
