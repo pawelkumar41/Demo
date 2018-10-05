497aae3594630d4f6271684164a69f779830888c
Pawel kumar <pawel.kumar@revv.co.in>
2018-10-05 12:20:38 +0530

    booking dump

--- src/main/java/com/util/Dump.java
+++ src/main/java/com/util/Dump.java
@@ -1,6 +1,8 @@
 package com.util;
 
+import com.common.Logins;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,6 +24,7 @@ public class Dump {
 
     @BeforeTest
     public static void login() throws Exception {
+        Logins.main();
         input = new FileInputStream("config.properties");
         // load a properties file
         prop.load(input);
@@ -77,7 +80,7 @@ public class Dump {
 
         {
             httpClient = HttpClients.createDefault();
-            HttpPost postRequest = new HttpPost(prop.getProperty(sActive)+"/api/admin/getDriverInOutTime");
+            HttpPost postRequest = new HttpPost(prop.getProperty(sActive) + "/api/admin/getDriverInOutTime");
             postRequest.addHeader("content-type", "application/json");
             JSONObject object = new JSONObject();
             object.put("email", "pawel.kumar@revv.co.in");
@@ -91,25 +94,57 @@ public class Dump {
             HttpResponse response = httpClient.execute(postRequest);
             System.out.println(response);
             int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode==200){
+            if (statusCode == 200) {
                 System.out.println(statusCode);
                 System.out.println("Success");
-            }
-            else if(statusCode != 200){
+            } else if (statusCode != 200) {
                 throw new RuntimeException("Failed with HTTP error code : " + statusCode);
             }
 
-        }
-        finally
+        } finally
 
         {
             httpClient.close();
             //Important: Close the connect
             // httpClient.getConnectionManager().shutdown();
         }
-
-
     }
+
+        @Test(priority = 3)
+        public static void getbookingDump() throws Exception {
+
+            try
+
+            {
+                httpClient = HttpClients.createDefault();
+                HttpGet getRequest = new HttpGet("http://35.154.151.188:3333/api/admin/"+Logins.adminid+"/"+Logins.accessToken+"/3/getBookingSumPanel?searchKey=%7B%22startDate%22:%222018-10-02T18:30:00.000Z%22,%22endDate%22:%222018-10-03T18:30:00.000Z%22%7D");
+                getRequest.addHeader("content-type", "application/json");
+                JSONObject object = new JSONObject();
+                String message;
+                message = object.toString();
+                System.out.println(message);
+                HttpResponse response = httpClient.execute(getRequest);
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode==200){
+                    System.out.println(statusCode);
+                    System.out.println("Success");
+                }
+                else if(statusCode != 200){
+                    throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+                }
+
+            }
+            finally
+
+            {
+                httpClient.close();
+                //Important: Close the connect
+                // httpClient.getConnectionManager().shutdown();
+            }
+
+
+
+        }
 
 
 
