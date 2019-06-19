0194c656467640e3f227d18cbf8ea5b3b1f7482c
Pawel kumar <pawel.kumar@revv.co.in>
2019-06-19 17:30:26 +0530

    tests

--- src/main/java/com/util/Registration.java
+++ src/main/java/com/util/Registration.java
@@ -470,16 +470,15 @@ public class Registration {
     }
 
 
-
     @Test
-    public static void goldInstallmentSubmission() throws Exception {
+    public static void InsquiryInstallmentDetail() throws Exception {
 
-        //Gold Installment Transaction
+        //Inquiry installment detaisl
         try
 
         {
             httpClient = HttpClients.createDefault();
-            HttpPost postRequest = new HttpPost("http://staging-b2b.emasdigi.com/api/vendor/v1/account/139/transaction/gold/installment");
+            HttpPost postRequest = new HttpPost("http://staging-b2b.emasdigi.com/api/vendor/v1/account/739/transaction/gold/installment/transaction_number");
             postRequest.addHeader("content-type", "application/json");
             postRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
             postRequest.addHeader("User-Agent", "vendor-id:7");
@@ -510,4 +509,300 @@ public class Registration {
             // httpClient.getConnectionManager().shutdown();
         }
     }
+
+
+    @Test
+    public static void inquiryInstallmentDetail() throws Exception {
+
+        //Transaction Status
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/account/739/transaction/gold/installment/123456");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+    @Test
+    public static void installmentPayment() throws Exception {
+
+        //Installment payment
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpPost postRequest = new HttpPost("http://staging-b2b.emasdigi.com/api/vendor/v1/account/739/transaction/gold/installment/1234");
+            postRequest.addHeader("content-type", "application/json");
+            postRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            postRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            object.put("reference", "REF-0001");
+            object.put("amount", 1);
+            object.put("unit_price", 582330);
+            object.put("currency", "IDR");
+            object.put("signature", "{{signature_local}}");
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(postRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+    @Test
+    public static void totalGoldSell() throws Exception {
+
+        //Cumulative Sell Transactions
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/account/739/transaction/gold/totalSell");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+    @Test
+    public static void termsAndConditions() throws Exception {
+
+        //Display terms and conditions
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/termsandconditions");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+
+    @Test
+    public static void installmentGoldAmountOptions() throws Exception {
+
+        //Installment Gold Amount Options
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/option/goldinstallment");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+
+    @Test
+    public static void installmentGoldTenumreOptions() throws Exception {
+
+        //Display Gold tenure option
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/option/tenure");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+    @Test
+    public static void kycOption1() throws Exception {
+
+        //Display KYC option 1
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/option/kyc1");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
+
+    @Test
+    public static void kycOption2() throws Exception {
+
+        //Display KYC option 2
+        try
+
+        {
+            httpClient = HttpClients.createDefault();
+            HttpGet getRequest = new HttpGet("http://staging-b2b.emasdigi.com/api/vendor/v1/option/kyc2");
+            getRequest.addHeader("content-type", "application/json");
+            getRequest.addHeader("api-key", "GaPWnbVwMKI5j7s7fEQtaF5S2Voe5Ix");
+            getRequest.addHeader("User-Agent", "vendor-id:7");
+            JSONObject object = new JSONObject();
+            String message;
+            message = object.toString();
+            //getRequest.setEntity(new StringEntity(message, "UTF8"));
+            HttpResponse response = httpClient.execute(getRequest);
+            System.out.println(response);
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 200) {
+                throw new RuntimeException("Failed with HTTP error code : " + statusCode);
+            }
+            System.out.println(statusCode);
+            BufferedReader br = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+            String output;
+
+        } finally
+
+        {
+            httpClient.close();
+            //Important: Close the connect
+            // httpClient.getConnectionManager().shutdown();
+        }
+    }
+
 }
